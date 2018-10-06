30061ce4369c15c25da2ceb0ee3d333b16ba9bd5
smheffron <smhgzd@mail.missouri.edu>
2018-10-06 17:53:14 -0500

    Added basic functionality

--- app/src/main/java/com/dine/dinendash/dinendash/MainActivity.java
+++ app/src/main/java/com/dine/dinendash/dinendash/MainActivity.java
@@ -3,11 +3,18 @@ package com.dine.dinendash.dinendash;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.navigation.Navigation;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
     }
 }
