655713ed465d200e649187446ee07ffa33322d61
jeffrohlman <jcrp9f@mail.missouri.edu>
2018-10-09 14:10:20 -0500

    Age is something that doesn't matter, unless you are a cheese. - Luis Bunuel

--- app/src/main/java/com/dine/dinendash/dinendash/models/Receipt.java
+++ app/src/main/java/com/dine/dinendash/dinendash/models/Receipt.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 
 public class Receipt {
     private ArrayList<ReceiptItem> items;
+    private double subTotal;
+    private double totalTax;
+    private double total;
 
     public Receipt() {
-
+        items = null;
+        subTotal = -1;
+        totalTax = 0;
+        total = 0;
     }
 
     public void AddItem(ReceiptItem item) {
@@ -24,4 +30,25 @@ public class Receipt {
     public void setItems(ArrayList<ReceiptItem> items) {
         this.items = items;
     }
+
+    public void setSubTotal(double subTotal){ this.subTotal = subTotal; }
+
+    public void setTotal(double Total){ this.total = Total; }
+
+    public void setTotalTax(double totalTax){ this.totalTax = totalTax; }
+
+    /* Add later for tax and tip per item instead of per user
+    public void calcTax(){
+        double runningTotal = 0;
+        double itemTax = 0;
+        if(items != null && totalTax != 0 && subTotal != 0){
+            for(int i = 0; i < items.size(); i++){
+                runningTotal += ((itemTax = items.get(i).getPrice() / subTotal)*totalTax);
+                items.get(i).setTax(itemTax);
+            }
+            if(runningTotal != totalTax){
+                items.get(items.size() - 1).setTax(items.get(items.size() - 1).getTax() + totalTax - runningTotal);
+            }
+        }
+    }*/
 }
--- app/src/main/java/com/dine/dinendash/dinendash/models/ReceiptItem.java
+++ app/src/main/java/com/dine/dinendash/dinendash/models/ReceiptItem.java
@@ -3,6 +3,8 @@ package com.dine.dinendash.dinendash.models;
 public class ReceiptItem {
     private String name;
     private double price;
+    //private double tax;
+    //private double tip;
 
     public  ReceiptItem() {
         this("", 0.0);
@@ -11,6 +13,8 @@ public class ReceiptItem {
     public ReceiptItem(String name, double price) {
         this.name = name;
         this.price = price;
+        //this.tax = 0;
+        //this.tip = 0;
     }
 
     public String getName() {
@@ -28,4 +32,21 @@ public class ReceiptItem {
     public void setPrice(double price) {
         this.price = price;
     }
+
+    /* Add these later if we decide to calculate tax and tip per item instead of per user
+    public double getTax() {
+        return tax;
+    }
+
+    public double getTip() {
+        return tip;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public void setTip(double tip) {
+        this.tip = tip;
+    }*/
 }
