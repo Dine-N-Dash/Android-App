8c5def3c5737a66a0791c913ac05bec0f1a82b3c
smheffron <smhgzd@mail.missouri.edu>
2018-11-18 15:19:13 -0600

    Allow for Paypal.me username change

--- app/src/main/java/com/dine/dinendash/dinendash/fragments/Settings.java
+++ app/src/main/java/com/dine/dinendash/dinendash/fragments/Settings.java
@@ -1,10 +1,8 @@
 package com.dine.dinendash.dinendash.fragments;
 
-import androidx.lifecycle.ViewModelProviders;
-import androidx.databinding.DataBindingUtil;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +11,14 @@ import com.dine.dinendash.dinendash.R;
 import com.dine.dinendash.dinendash.databinding.FragmentSettingsBinding;
 import com.dine.dinendash.dinendash.viewModels.SettingsViewModel;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
 public class Settings extends Fragment {
     private SettingsViewModel viewModel;
+    private String username;
 
     public Settings() {
         // Required empty public constructor
@@ -34,7 +38,26 @@ public class Settings extends Fragment {
         View view = binding.getRoot();
         binding.setViewModel(viewModel);
         binding.setFragment(this);
+
+        SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(getActivity());
+        username = sharedPreferences.getString("username", "");
+        viewModel.setUsername(username);
         return view;
+    }
+
+    public void setUsername(String username) {
+        viewModel.setUsername(username);
+    }
+
+    public void onUsernameChangeConfirmed(String newUsername) {
+        SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(getActivity());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("username", newUsername);
+        editor.apply();
+
+        setUsername(newUsername);
     }
 
 }
