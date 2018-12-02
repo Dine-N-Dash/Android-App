ac8c2e150fa3e823ca5dbe288c9dcff80bae9e18
ryanfilkins97 <rmfvg5@mail.missouri.edu>
2018-12-01 18:09:39 -0600

    Unneeded images will now be removed from app storage on startup.

--- app/src/main/java/com/dine/dinendash/dinendash/activities/MainActivity.java
+++ app/src/main/java/com/dine/dinendash/dinendash/activities/MainActivity.java
@@ -1,8 +1,14 @@
 package com.dine.dinendash.dinendash.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
 import com.dine.dinendash.dinendash.R;
+
+import java.io.File;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +16,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Delete unneeded files
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        if (storageDir != null) {
+            File[] files = storageDir.listFiles();
+
+            for (File f: files) {
+                if (!f.delete()) {
+                    Log.e("PHOTO-DELETE", "Unable to delete: " + f.getAbsolutePath());
+                }
+            }
+        }
+
         setContentView(R.layout.activity_main);
     }
 
